670dd99b9a4a5ebece21f9b185b59c505b15253f
Luis Angel Mendoza Lucio <lmendoza@raudalfootwear.com.mx>
2026-09-15 17:07:46 -0600

    modelo codigo autorizacion

---
+++ Calidad_API/Calidad_API/Models/CodigoAutorizacion.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Calidad_API.Models;
+
+[Table("codigo_autorizacion")]
+public class CodigoAutorizacion
+{
+    [Key]
+    [Column("id_codigo_autorizacion")]
+    public short IdCodigoAutorizacion { get; set; }
+    
+    [Column("is_usuario")]
+    public short IdUsuario { get; set; }
+    
+    [Column("codigo_hash")]
+    public string CodigoHash { get; set; }
+    
+    [Column("activo")]
+    public bool Activo { get; set; }
+    
+    [Column("fecha_expiracion")]
+    public DateTime FechaCreacion { get; set; }
+    
+    [Column("fecha_expiracion")]
+    public DateTime FechaExpiracion { get; set; }
+}
